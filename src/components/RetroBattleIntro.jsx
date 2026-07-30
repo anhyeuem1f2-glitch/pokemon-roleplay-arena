1187f63a1033d15e8348a5e3779a019d1c960aea
anhyeuem1f2-glitch <anhyeuem1f2@gmail.com>
2026-07-30 11:34:35 +0700

    Add files via upload

--- src/components/RetroBattleIntro.jsx
+++ src/components/RetroBattleIntro.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
 
-const frames = [
-  '/retro-intro/frame1.png',
-  '/retro-intro/frame2.png',
-  '/retro-intro/frame3.png',
-]
-
 export default function RetroBattleIntro({ active, dimmed }) {
   return (
     <div className={`retro-intro ${active ? 'retro-intro--active' : ''} ${dimmed ? 'retro-intro--dimmed' : ''}`} aria-hidden="true">
       <div className="retro-intro__shell">
         <div className="retro-intro__screen">
-          {frames.map((src, index) => (
-            <img
-              key={src}
-              src={src}
-              alt=""
-              className={`retro-intro__frame retro-intro__frame--${index + 1}`}
-              draggable="false"
-            />
-          ))}
-          <div className="retro-intro__flash" />
-          <div className="retro-intro__vignette" />
+          <img
+            src="/retro-intro/intro-battle.webp"
+            alt=""
+            className="retro-intro__media"
+            draggable="false"
+            loading="eager"
+          />
+          <div className="retro-intro__scanlines" />
         </div>
       </div>
     </div>
